b48e47c4431767b3b5ee23d5a174ef201b26d4fc
Alvaro Separovich Hamalay <alvasepro@gmail.com>
2021-06-03 12:01:54 -0300

    FEAT: tema preto

--- src/sidebar/SidebarBlock.tsx
+++ src/sidebar/SidebarBlock.tsx
@@ -6,7 +6,7 @@ type ISidebarBlock = {
 };
 
 const SidebarBlock = (props: ISidebarBlock) => (
-  <div className="sidebar-block bg-white shadow p-6 my-10 lg:p-8 md:first:mt-0">
+  <div className="sidebar-block shadow p-6 my-10 lg:p-8 md:first:mt-0">
     <div className="text-lg">{props.title}</div>
 
     <div className="mt-4">{props.children}</div>
